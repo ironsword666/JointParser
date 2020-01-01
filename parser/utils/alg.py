838f8ba479e86f70ea05e5f7552eb4463a345c41
zysite <yzhang.cs@outlook.com>
2020-01-01 22:48:57 +0800

    Remove unused modules

--- parser/utils/alg.py
+++ parser/utils/alg.py
@@ -4,7 +4,6 @@ from parser.utils.fn import stripe
 
 import torch
 import torch.autograd as autograd
-from torch.nn.utils.rnn import pad_sequence
 
 
 def kmeans(x, k):
@@ -133,67 +132,3 @@ def cky(scores, mask):
              for i, length in enumerate(lens.tolist())]
 
     return trees
-
-
-def eisner(scores, mask):
-    lens = mask.sum(1)
-    batch_size, seq_len, _ = scores.shape
-    scores = scores.permute(2, 1, 0)
-    s_i = torch.full_like(scores, float('-inf'))
-    s_c = torch.full_like(scores, float('-inf'))
-    p_i = scores.new_zeros(seq_len, seq_len, batch_size).long()
-    p_c = scores.new_zeros(seq_len, seq_len, batch_size).long()
-    s_c.diagonal().fill_(0)
-
-    for w in range(1, seq_len):
-        n = seq_len - w
-        starts = p_i.new_tensor(range(n)).unsqueeze(0)
-        # ilr = C(i->r) + C(j->r+1)
-        ilr = stripe(s_c, n, w) + stripe(s_c, n, w, (w, 1))
-        # [batch_size, n, w]
-        ilr = ilr.permute(2, 0, 1)
-        il = ilr + scores.diagonal(-w).unsqueeze(-1)
-        # I(j->i) = max(C(i->r) + C(j->r+1) + s(j->i)), i <= r < j
-        il_span, il_path = il.max(-1)
-        s_i.diagonal(-w).copy_(il_span)
-        p_i.diagonal(-w).copy_(il_path + starts)
-        ir = ilr + scores.diagonal(w).unsqueeze(-1)
-        # I(i->j) = max(C(i->r) + C(j->r+1) + s(i->j)), i <= r < j
-        ir_span, ir_path = ir.max(-1)
-        s_i.diagonal(w).copy_(ir_span)
-        p_i.diagonal(w).copy_(ir_path + starts)
-
-        # C(j->i) = max(C(r->i) + I(j->r)), i <= r < j
-        cl = stripe(s_c, n, w, (0, 0), 0) + stripe(s_i, n, w, (w, 0))
-        cl_span, cl_path = cl.permute(2, 0, 1).max(-1)
-        s_c.diagonal(-w).copy_(cl_span)
-        p_c.diagonal(-w).copy_(cl_path + starts)
-        # C(i->j) = max(I(i->r) + C(r->j)), i < r <= j
-        cr = stripe(s_i, n, w, (0, 1)) + stripe(s_c, n, w, (1, w), 0)
-        cr_span, cr_path = cr.permute(2, 0, 1).max(-1)
-        s_c.diagonal(w).copy_(cr_span)
-        s_c[0, w][lens.ne(w)] = float('-inf')
-        p_c.diagonal(w).copy_(cr_path + starts + 1)
-
-    def backtrack(p_i, p_c, heads, i, j, complete):
-        if i == j:
-            return
-        if complete:
-            r = p_c[i, j]
-            backtrack(p_i, p_c, heads, i, r, False)
-            backtrack(p_i, p_c, heads, r, j, True)
-        else:
-            r, heads[j] = p_i[i, j], i
-            i, j = sorted((i, j))
-            backtrack(p_i, p_c, heads, i, r, True)
-            backtrack(p_i, p_c, heads, j, r + 1, True)
-
-    predicts = []
-    p_c = p_c.permute(2, 0, 1).cpu()
-    p_i = p_i.permute(2, 0, 1).cpu()
-    for i, length in enumerate(lens.tolist()):
-        heads = p_c.new_ones(length + 1, dtype=torch.long)
-        backtrack(p_i[i], p_c[i], heads, 0, length, True)
-        predicts.append(heads.to(mask.device))
-
-    return pad_sequence(predicts, True)
